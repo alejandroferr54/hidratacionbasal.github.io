71779eabf789556ef9cfb448bc940fd3352bc2c7
Luis Ferreira <48211913+alejandroferr54@users.noreply.github.com>
2024-02-21 09:40:46 -0300

    Corección Syntaxis.js
    + Titulo del resultado, o nombre.
    + Se corrigen errores de syntax. (O mala práctica)

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
 const MOR = document.getElementById('calculadura__caso2');
 const MAR = document.getElementById('calculadura__caso3');
-
+const MER = document.getElementById('calculadura__caso1');
 
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value
@@ -23,12 +23,14 @@ CALCULAR.addEventListener('click', () => {
         if (DATO > 30) {
             //console.log(DATO)
             //console.log(`${flujo} EL FLUJO`)
+            MER.innerHTML = 'Caso de Superficie Corporal';
             let valor1 = flujo * 1500
             let mantenimiento1 = Math.round((valor1/24)*1.5);
             FLU.innerHTML =  Math.round((valor1/24)) + ' cc/hr';
             MAN.innerHTML = 'm+m/2 ' + mantenimiento1 + ' cc/hr';
             FLU.style.display = 'block';
             MAN.style.display = 'block';
+            MER.style.display = 'block';
 
             // Datos por 2litros de agua
             let valor2 = flujo * 2000
@@ -39,6 +41,8 @@ CALCULAR.addEventListener('click', () => {
             MAR.style.display = 'block';
             //console.log(`VALORES  ${valor1} a ${valor2}`)
         } else {
+            MER.innerHTML = 'Caso de Holly Day'
+            MER.style.display = 'block';
             // Metodo de HollyDay
             let mantenimiento = Math.round(flujo*1.5);
             FLU.innerHTML = flujo + ' cc/hr';
@@ -61,22 +65,22 @@ CALCULAR.addEventListener('click', () => {
 function calculeHidrate ( kg ) {
     if (kg){
         if (kg <= 30) {
-            let cc = hollydaySegar(kg)
-            return Math.round(cc/24)
+            let cc = hollydaySegar(kg);
+            return Math.round(cc/24);
             //return console.log(`${cc}cc, ${cc/24}cc/hr, ${(cc/24) * 1.5}m+m/2 `)
         } else if (kg > 30){
-            return superficiecorporal(kg)
+            return superficiecorporal(kg);
         }
     }
 }
 
 // Funcion de la Superficie Corporal ((x * 4 + 7) / x + 90)
 function superficiecorporal(kg) {
-    let c = parseInt(kg)
-    console.log(c+90)
-    c = ((c *4)+7)/(c + 90)
-    console.log(c)
-    return c
+    let c = parseInt(kg);
+    console.log(c+90);
+    c = ((c *4)+7)/(c + 90);
+    console.log(c);
+    return c;
 
 }
 
@@ -84,11 +88,14 @@ function superficiecorporal(kg) {
 
 //  ----------------------------------------------------------------
 // Funcion propia, método de HollyDay
+// Si desean probarlo esta disponible en 
+// https://alejandroferr54.github.io/hidratacionbasal.github.io/
+// Mi repositorio disponible en https://github.com/alejandroferr54
 function hollydaySegar(kg) {
     // Parseando a entero para no tener problemas aritmeticos. (el value es una string.)
-    let peso = parseInt(kg)
+    let peso = parseInt(kg);
     //Defino el cálculo
-    let cc = 0
+    let cc = 0;
     // Si el peso es mayor a veinte se ejecuta el código
     // Caso contrario continua con la proxima condicional.
     if (peso > 20) {
@@ -96,7 +103,7 @@ function hollydaySegar(kg) {
         // debe tomar 1500cc que es un litro y medio de agua, entonces
         // Se suma la diferencia del peso y se multiplica por 20
         // Ejp: Si el peso es 25, entonces 1500 + 5 x 20, que seria 1600cc
-        cc = 1500 + ((peso - 20) * 20)
+        cc = 1500 + ((peso - 20) * 20);
 
         // Retorna el valor resultante
         return cc
@@ -104,14 +111,14 @@ function hollydaySegar(kg) {
         // Si el peso es mayor a 10 y menor o igual a 20, se asume que 
         // minimamente debe tomar 1l de agua, en ese caso se multiplica
         // la diferencia x 50 y se suma por 1000cc
-            cc = 1000 + ((peso - 10) * 50)
+            cc = 1000 + ((peso - 10) * 50);
 
             // Se retorna el valor resultante
-            return cc
+            return cc;
     }  else if (peso <=10){
         // Si el peso es menor o igual a 10, se multiplica por 100
-        cc = peso * 100
+        cc = peso * 100;
         // Se retorna el valor resultante
-            return cc
+            return cc;
     }
 }
